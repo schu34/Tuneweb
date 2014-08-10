1144a93640a3015c9c3e3f2f568b687efafb59ad
Matthew Schupack <schu34@gmail.com>
2014-08-10 12:07:48 -0700

    got rid of arrows, changed transitions

--- client/js/app.js
+++ client/js/app.js
@@ -4,67 +4,13 @@ var nodes = [];
 
 var graph = new Springy.Graph();
 
-var numReasults = 5 //number of reasults 
+var numReasults = 5; //number of reasults 
 
 $("#my_canvas").springy({
     graph: graph
 });
 
-var testData = {
-    "response": {
-        "artists": [{
-            "id": "ARYJFJ91187B9B149A",
-            "name": "The Wilderness"
-        }, {
-            "id": "ARGOAEZ122988F2731",
-            "name": "Big a"
-        }, {
-            "id": "AREBMWM11F50C47FFC",
-            "name": "Fantastic Four"
-        }, {
-            "id": "ARGMRZU1187B9B6981",
-            "name": "The Heaters"
-        }, {
-            "id": "ARDLOZD1187B9B98A8",
-            "name": "Boundless"
-        }, {
-            "id": "ARVXBOT1187B992D93",
-            "name": "City to City"
-        }, {
-            "id": "ARUAQW51187B994266",
-            "name": "Private Dancer"
-        }, {
-            "id": "ARSBVCE1187B99BE74",
-            "name": "Hearts of Stone"
-        }, {
-            "id": "ARCF56Q1187FB55282",
-            "name": "Airliner"
-        }, {
-            "id": "ARX75EF1187B9A272F",
-            "name": "Loom"
-        }, {
-            "id": "ARBCWYF1187B9953F8",
-            "name": "Lavish"
-        }, {
-            "id": "ARFPFW61187B992B5B",
-            "name": "Insatiable"
-        }, {
-            "id": "ARIYZVN1269FCD068A",
-            "name": "The Foreground"
-        }, {
-            "id": "ARGQULJ12086C11996",
-            "name": "Rhode Island"
-        }, {
-            "id": "ARYHWYN1187B989C56",
-            "name": "Eleventh Hour"
-        }],
-        "status": {
-            "code": 0,
-            "message": "Success",
-            "version": "4.2"
-        }
-    }
-};
+
 
 
 var explore = function() {
@@ -95,11 +41,12 @@ var fetchRelated = function(artist) {
     //perform the query
 
     $.getJSON(url, args, function(json, textStatus) {
-        $(".center").slideUp("slow");
-        $("#my_canvas").slideDown("slow");
+        $(".center").fadeOut("slow");
+        $(".top_search").fadeIn("slow");
+        $("#my_canvas").fadeIn("slow");
         $("html, body").animate({
             scrollTop: $('#my_canvas').offset().top
-        }, "slow")
+        }, "slow");
         updateGraph(artist, json);
     });
 };
@@ -134,6 +81,7 @@ var updateGraph = function(artist, data) {
 
 $(document).ready(function($) {
     $("#my_canvas").hide();
+    $(".top_search").hide();
     $("button").on("click", explore);
 
 
