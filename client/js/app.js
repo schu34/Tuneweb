40f61a78eb932c4e28e05bf3878f78a32d1f55f9
Matthew Schupack <schu34@gmail.com>
2016-11-29 14:01:12 -0600

    refactor

--- client/js/app.js
+++ client/js/app.js
@@ -1,20 +1,8 @@
 var numResults = 5; //number of results this needs to be user setable
 
-var width, height;
-
-
-var simulation;
-
-var graph = {
-    nodes: [],
-    links: []
-};
-
-
-
 //gets called each time there is a search
 var explore = function() {
-    clearGraph();
+    graph.clear();
     var query = {};
     query.artist = $(".artist").val() || $(".center .artist").val();
     query.artist = utils.titleCase(query.artist);
@@ -23,7 +11,7 @@ var explore = function() {
 
     if (query.artist.length > 0)
         fetchRelated(query, function(artists) {
-            updateGraph(query.artist, artists);
+            graph.update(query.artist, artists);
             transition();
         });
 };
@@ -32,104 +20,6 @@ var fetchRelated = function(query, callback) {
     $.post('/artist', query, function(data) {
         callback(data);
     });
-};
-
-
-//updates the graph
-var updateGraph = function(artist, data) {
-  graph.nodes.push({id: artist});
-    console.log("data: " + data);
-    newNodes = data.map(function(a) {
-        return {
-            id: a.name,
-            group: 1
-        };
-    })
-
-    for (var i = 0; i < newNodes.length; i++) {
-        artistIdx = graph.nodes.indexOf(newNodes[i]);
-        if (artistIdx !== -1) {
-            graph.links.push({
-                source: artist,
-                target: graph.nodes[artistIdx].id,
-                value: 50
-            })
-            newNodes.splice(i, 1);
-        }
-    }
-
-    for (var i = 0; i < newNodes.length; i++) {
-        graph.links.push({
-            source: artist,
-            target: newNodes[i].id,
-            value: 500
-        });
-    }
-    graph.nodes = graph.nodes.concat(newNodes);
-
-    var link = d3.select("#links")
-        .selectAll("line")
-        .data(graph.links)
-        .enter().append("line")
-        .attr("stroke-Width", function(d) {
-            return Math.sqrt(d.value);
-        });
-
-    var node = d3.select("#nodes")
-        .selectAll("circle")
-        .data(graph.nodes)
-        .enter().append("circle")
-        .attr("r", 10)
-        .call(d3.drag()
-            .on("start", dragStarted)
-            .on("drag", dragged)
-            .on("end", dragEnded));
-
-    node.append("title").text(function(d) {
-        return d.id;
-    });
-
-    simulation
-        .nodes(graph.nodes)
-        .on("tick", ticked);
-
-    simulation
-        .force("link")
-        .links(graph.links);
-
-  function ticked() {
-    link
-      .attr("x1", function(d){return d.source.x;})
-      .attr("y1", function(d){return d.source.y;})
-      .attr("x2", function(d){return d.target.x;})
-      .attr("y2", function(d){return d.target.y;});
-
-    node
-      .attr("cx", function(d){return d.x})
-      .attr("cy", function(d){return d.y});
-  }
-};
-
-function dragStarted(d){
-  if(!d3.event.active) simulation.alphaTarget(0.3).restart();
-  d.fx = d.x;
-  d.dy = d.y;
-}
-
-function dragged(d){
-  d.fx = d3.event.x;
-  d.fy = d3.event.y;
-}
-
-function dragEnded(d){
-  if(!d3.event.active) simulation.alphaTarget(0);
-  d.fx = null;
-  d.fy = null;
-}
-
-//clears the graph on the screen and resets the nodes array to []
-var clearGraph = function() {
-
 };
 
 
@@ -162,17 +52,5 @@ $(document).ready(function() {
         }
     });
 
-
-
-    svg = d3.select("svg")
-    width = +svg.attr("width");
-    height = +svg.attr("height");
-
-    simulation = d3.forceSimulation()
-        .force("link", d3.forceLink().id(function(d) {
-            return d.id
-        }))
-        .force("charge", d3.forceManyBody())
-        // .force("center", d3.forceCenter(width / 2, height / 2))
-
+    graph.init();
 });
---
+++ client/js/graph.js
@@ -0,0 +1,146 @@
+var graph = {
+    nodes: [],
+    links: [],
+    svg: null,
+    simulation: null,
+    zoom: null,
+    width: 0,
+    height: 0,
+    centerX: 0,
+    centerY: 0,
+
+    init: function(){
+        this.svg = d3.select("svg")
+        this.width = +this.svg.attr("width");
+        this.height = +this.svg.attr("height");
+        this.centerX = this.width/2;
+        this.centerY  = this.height/2;
+
+        this.simulation = d3.forceSimulation()
+            .force("link", d3.forceLink().id(function(d) {
+                return d.id
+            }))
+            .force("charge", d3.forceManyBody())
+            .force("center", d3.forceCenter(this.centerX, this.centerY))
+        this.zoom = d3.zoom().on("zoom", this.zoomed);
+        this.svg.call(this.zoom)
+
+    },
+    nodeDragStarted: function(d){
+        if (!d3.event.active) graph.simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.dy = d.y;
+    },
+    nodeDragged: function(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    },
+    nodeDragEnded: function(d){
+        if (!d3.event.active) graph.simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+    },
+
+
+    zoomed: function(){
+        d3.select(".container").attr("transform", "translate("+ d3.event.transform.x + "," + d3.event.transform.y + ")scale(" + d3.event.transform.k + ")");
+    },
+    clear: function(){},
+    update: function(artist, data) {
+        this.updateModel(artist, data);
+        this.updateView();
+    },
+
+    updateModel: function(artist, data) {
+        this.nodes.push({
+            id: artist
+        });
+        console.log("data: " + data);
+        newNodes = data.map(function(a) {
+            return {
+                id: a.name,
+                group: 1
+            };
+        })
+
+        for (var i = 0; i < newNodes.length; i++) {
+            artistIdx = this.nodes.indexOf(newNodes[i]);
+            if (artistIdx !== -1) {
+                this.links.push({
+                    source: artist,
+                    target: this.nodes[artistIdx].id,
+                    value: 50
+                })
+                newNodes.splice(i, 1);
+            }
+        }
+
+        for (var i = 0; i < newNodes.length; i++) {
+            this.links.push({
+                source: artist,
+                target: newNodes[i].id,
+                value: 500
+            });
+        }
+        this.nodes = this.nodes.concat(newNodes);
+    },
+
+    updateView: function() {
+        var link = d3.select("#links")
+            .selectAll("line")
+            .data(this.links)
+            .enter().append("line")
+            .attr("stroke-Width", function(d) {
+                return Math.sqrt(d.value);
+            });
+
+        var node = d3.select("#nodes")
+            .selectAll("text")
+            .data(this.nodes)
+            .enter().append("text")
+            .text(function(d){return d.id;})
+            .call(d3.drag()
+                .on("start", this.nodeDragStarted)
+                .on("drag", this.nodeDragged)
+                .on("end", this.nodeDragEnded));
+
+        node.append("title").text(function(d) {
+            return d.id;
+        });
+
+
+        this.simulation
+            .nodes(this.nodes)
+            .on("tick", ticked);
+
+        this.simulation
+            .force("link")
+            .links(this.links);
+
+        this.simulation.restart();
+
+        function ticked() {
+            link
+                .attr("x1", function(d) {
+                    return d.source.x;
+                })
+                .attr("y1", function(d) {
+                    return d.source.y;
+                })
+                .attr("x2", function(d) {
+                    return d.target.x;
+                })
+                .attr("y2", function(d) {
+                    return d.target.y;
+                });
+
+            node
+                .attr("x", function(d) {
+                    return d.x
+                })
+                .attr("y", function(d) {
+                    return d.y
+                });
+        }
+    }
+};
